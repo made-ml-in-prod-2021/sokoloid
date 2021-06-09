b2da76d67e57f7d46b4ce0ef43bbca4246405c40
Alexandr Sokolov <aesokolov1975@gmail.com>
2021-06-09 23:14:19 +0300

    hw3

---
+++ airflow_ml_dags/images/hw03-make-data/make_data.py
@@ -0,0 +1,24 @@
+import os
+from sklearn.datasets import make_blob
+
+import click
+
+X_FILENAME = data.csv
+Y_FILENAME = target.csv
+
+@click.command("make_data")
+@click.option("--output-dir")
+def make_data(output_dir: str, output_dir):
+    X, y = make_blobs(n_samples=100, centers=3, n_features=2,)
+    with open(os.path.join(output_dir, X_FILENAME), "wt") as f_out:
+        for x_ in X:
+            f_out.write(f"{x_}\n")
+
+    with open(os.path.join(output_dir, Y_FILENAME), "wt") as f_out:
+        for y_ in y:
+            f_out.write(f"{y_}\n")
+
+
+
+if __name__ == '__main__':
+    make_data()
